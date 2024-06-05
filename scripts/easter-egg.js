50606881e737b1654d9510db9c03036ca5bfca25
v1tti <bielvitti@hotmail.com>
2024-06-05 17:34:43 -0300

    adicionando imagens para carrossel e melhorando script de easter egg

--- scripts/easter-egg.js
+++ scripts/easter-egg.js
@@ -2,29 +2,42 @@ const puzzlePiece = document.getElementById("puzzle-piece");
 const gifButton = document.getElementById("secret-disabled");
 
 let isDragging = false;
+let offsetX, offsetY;
 
-puzzlePiece.addEventListener("mousedown", () => {
+puzzlePiece.addEventListener("mousedown", (event) => {
   isDragging = true;
   puzzlePiece.classList.add("dragging");
+
+  offsetX = event.clientX - puzzlePiece.offsetLeft;
+  offsetY = event.clientY - puzzlePiece.offsetTop;
+
   gifButton.removeAttribute("disabled");
   gifButton.classList.remove("secret-disabled");
   gifButton.classList.add("secret-button");
   gifButton.id = "gif-button";
+
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
 });
 
-puzzlePiece.addEventListener("mouseup", () => {
+function onMouseMove(event) {
+  if (isDragging) {
+    puzzlePiece.style.left = event.clientX - offsetX + "px";
+    puzzlePiece.style.top = event.clientY - offsetY + "px";
+  }
+}
+
+function onMouseUp() {
   isDragging = false;
   puzzlePiece.classList.remove("dragging");
-  document.getElementById("gif-button").addEventListener("click", renderEasterEgg);
-});
 
-document.addEventListener("mousemove", (event) => {
-  if (isDragging) {
-    puzzlePiece.style.left = event.clientX - puzzlePiece.offsetWidth / 2 + "px";
-    puzzlePiece.style.top = event.clientY - puzzlePiece.offsetHeight / 2 + "px";
-  }
-});
+  document.removeEventListener("mousemove", onMouseMove);
+  document.removeEventListener("mouseup", onMouseUp);
+}
+
 function renderEasterEgg() {
   const gifContainer = document.getElementById("gif-container");
   gifContainer.classList.remove("hidden");
 }
+
+document.getElementById("secret-disabled").addEventListener("click", renderEasterEgg);
